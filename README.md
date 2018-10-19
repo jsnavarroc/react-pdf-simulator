9ccd078a4c8c3d81f714c7c06468af24fce2dad1
Joha Navarro <21sebas12@gmail.com>
2018-10-19 12:40:22 -0500

    Se actualizar el readme. ademas del componente

--- README.md
+++ README.md
@@ -2,34 +2,6 @@
 
 # Pdf Simulator
 
-# How to install yarn
-
-## Install yarn on Windows
-
-There are three options for installing Yarn on Windows, you can fine <a href="https://yarnpkg.com/lang/en/docs/install/#windows-stable" target="_blank">here</a> 
-
-## Install yarn on Debian / Ubuntu
-
-On Debian or Ubuntu Linux, you can install Yarn via our Debian package repository. You will first need to configure the repository
-
-`curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -`
-<br/>
-`echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list`
-
-Then you can simply:
-
-`sudo apt-get update && sudo apt-get install yarn`
-
-
-## Install yarn on Mac with Homebrew
-
-You can install Yarn through the Homebrew package manager. This will also install Node.js if it is not already installed.
-
-```
-brew install yarn
-```
-
-
 # How to run the app - Available Scripts
 In the project directory, you can run:
 
@@ -38,26 +10,15 @@ yarn start
 ```
 
 Runs the app in the development mode.
-Open http://localhost:3000 to view it in the browser.
+Open http://localhost:3001 to view it in the browser.
 
-The page will reload if you make edits.
-You will also see any lint errors in the console.
-
-```
-yarn test
-```
 
 Launches the test runner in the interactive watch mode.
 
 ```
-yarn build
+yarn prepublishOnly
 ```
 
-Builds the app for production to the build folder.
-
-```
-yarn eject
-```
 
 Note: this is a one-way operation. Once you eject, you can’t go back!
 If you aren’t satisfied with the build tool and configuration choices, you can eject at any time. This command will remove the single build dependency from your project.
@@ -90,3 +51,5 @@ Se verá así:
 ![alt text](https://image.ibb.co/k9w5y0/5.png)
 
 Los atributos que se pueden modificar son:
+
+Aún sin construir 
--- src/PDFsimulator/tools/styles/styles.js
+++ src/PDFsimulator/tools/styles/styles.js
@@ -1,87 +1,96 @@
-  export const stylesContainerView = ( ) => {
-
-    return {
-    stylesContainerView: {
-        backgroundColor: '#525659',
-        width:'990px',
-        height: '99vh',
-        minWidth:'400px',
-        margin: '8px',
-        justifyContent: 'center',
-        alignItems: 'center',
-        overflow: 'scroll',
-        resize: 'both',
-        display: 'block',
-    },
-    zoom:{
-      zoom: '1',
-    },
-    // Styles button
-    buttonConmon: {
-      marginBottom:'20px',
-    },
-    buttonContent:{
-      marginTop: '10vh',
-      marginBottom: '10vh',
-      position: 'fixed',
-    },
-    extendedIcon: {
-      marginRight: 8,
-    },
-    sidebar: {
-      position: 'sticky',
-      top: '15px',
-    },
-
-};};
-
-  export const stylesDataView = (  ) => ({
-    marginText : {
-      /*  Contenido  */
-      backgroundColor: 'white',
-       /*  Tamaño  */
-      margin:'39px auto',
-      marginBottom:'2vh',
-      minWidth:'818px',
+export const stylesContainerView = ( ) =>({
+  stylesContainerView: {
+      backgroundColor: '#525659',
+      width:'492px',
+      height: '88vh',
+      maxHeight:'88vh', 
+      minHeight:'88vh', 
       maxWidth:'818px',
-      height: '1055px',
-      // maxHeight: '1055px',
-      // /* Forma  */
-      // display: 'static',
-      // position: 'relative',
-      // Shadow
-      WebkitBoxShadow: '1px 1px 11px 1px rgba(0,0,0,0.66)',
-      MozBoxShadow: '1px 1px 11px 1px rgba(0,0,0,0.66)',
-      BoxShadow: '1px 1px 11px 1px rgba(0,0,0,0.66)',
-      overflow: 'hidden',
+      minWidth:'492px', 
+      margin: '8px',
+      justifyContent: 'center',
+      alignItems: 'center',
+      overflow: 'scroll',
+      resize: 'both',
+      display: 'block',
+      WebkitBoxShadow: '0px 2px 20px -2px rgba(0,0,0,0.58)',
+      MozBoxShadow: '0px 2px 20px -2px rgba(0,0,0,0.58)',
+      BoxShadow: '0px 2px 20px -2px rgba(0,0,0,0.58)',
+      BorderRadius: '20px',
+  },
+  zoom:{
+    zoom: '1',
+  },
+  controlMarging: {
+    height: '2px',
+  },
+  // Styles button
+  buttonConmon: {
+    marginBottom:'20px',
+  },
+  buttonContent:{
+    marginTop: '55vh',
+    marginBottom: '10vh', 
+    position: 'fixed',
+    marginLeft: '-7px',
+  },
+  extendedIcon: {
+    marginRight: '8px',
+  },
+  sidebar: {
+    position: 'sticky',
+    top: '15px',
+  },
 
-    },
-    stylesText : {
-      /*  Contenido  */
-      backgroundColor: 'white',
-       /*  Tamaño  */
-      margin:'39px auto',
-      marginBottom:'7.5vh',
-      minWidth:'818px',
-      maxWidth:'818px',
-      // height: '975px',
-      // maxHeight: '975px',//Esto es el alto maximo de la pagina
-      // /* Forma  */
-      // position: 'relative',
-      // Shadow
-      overflow: 'hidden',
-
-    },
+});
+//  Este es el css de la hoja
+export const stylesDataView = (  ) => ({
+  marginText : {
+    /*  Contenido  */
+    backgroundColor: 'white',
+     /*  Tamaño  */
+    margin:'-30px 0 30 0' ,
+    marginBottom:'2vh' ,
+    minWidth:'818px', 
+    maxWidth:'818px',
+    height: '1055px', 
+    // maxHeight: '1055px',
+    // /* Forma  */
+    // display: 'static',
+    // position: 'relative',
+    // Shadow
+    WebkitBoxShadow: '1px 1px 11px 1px rgba(0,0,0,0.66)',
+    MozBoxShadow: '1px 1px 11px 1px rgba(0,0,0,0.66)',
+    BoxShadow: '1px 1px 11px 1px rgba(0,0,0,0.66)',
+    overflow: 'hidden',
+    
+  },
+  stylesText : {
+    /*  Contenido  */
+    backgroundColor: 'white',
+     /*  Tamaño  */
+    margin:'39px auto' ,
+    marginBottom:'7.5vh' ,
+    // minWidth:'818px', 
+    // maxWidth:'818px',
+    // height: '975px', 
+    // maxHeight: '975px',//Esto es el alto maximo de la pagina
+    // /* Forma  */
+    // position: 'relative',
+    // Shadow
+    overflow: 'hidden',
+    
+  },
 });
 
 export const zoomCss = (num) => (
-    {
-      // zoom: `${num}`,
-      transform: `scale(${num})`,
-      margin:'59px auto',
-      /* Posicionamiento al momento de transformar */
-      WebkitTransformOrigin: '0% 0',
-      MozTransformOrigin: '0 0',
-      TransformOrigin: '0 0',
-    }
-);
+  {
+    // zoom: `${num}`,
+    transform: `scale(${num})`,
+    margin:'59px auto',
+    /* Posicionamiento al momento de transformar */
+    WebkitTransformOrigin: '0% 0',
+    MozTransformOrigin: '0 0',
+    TransformOrigin: '0 0',
+  }
+)
